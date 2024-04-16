8e2930fbf2baac3ed0738c1f57764856f56008d9
Diana Z <dianaiulianaivan@yahoo.com>
2024-04-16 20:20:28 +0300

    curs 3 - operatori

---
+++ curs3/index.php
@@ -0,0 +1,108 @@
+<?php
+$a = 5;
+$b = 7;
+$c = 2; 
+$x = ($b-$a)*$c;
+$y = $x%$c;
+echo $x;
+echo '<br/>';
+echo $y;
+echo '<br/>';
+echo $x = $a+$b/$c;
+echo '<br/>';
+echo $x +=10; 
+echo '<br/>';
+// += $a +=1 inseamna $a=$a+1;
+// alt exemplu $a +=7;-> $a=$a+7; 
+// -= $a -=3; ->$b = $b-3;
+echo $a +=1;
+echo '<br/>';
+echo $b -=3;
+echo '<br/>';
+$t = 'text1';
+$z = 'z';
+echo $z.=$t;
+echo '<br/>';
+$x = (1 > 2);
+var_dump ($x);
+echo '<br/>';
+$x = (3 ==3);
+var_dump ($x);
+echo '<br/>';
+$x = ('3'==3);
+var_dump ($x);
+echo '<br/>';
+$x = ('3'===3); //intre ghilimele tip string; fara ghilimele tip int; 
+var_dump ($x);
+echo '<br/>';
+$x = ('3'!==3);
+var_dump ($x);
+echo '<br/>';
+$x = (1!=2);
+var_dump ($x);
+echo '<br/>';
+$x = (3>=4);
+var_dump ($x); // diferenta intre comenzile echo (doar iti afiseaza valoarea) si var dump (pe langa valoare iti da si tip); 
+echo '<br/>';
+// operatori incrementare 
+$j = 5;
+var_dump ($j++);
+echo '<br/>';
+$i = 7;
+var_dump (--$i);
+echo '<br/>';
+echo $j;
+echo '<br/>';
+// operatori logici - se folosesc in if uri
+// && -> si 
+// || -> sau 
+// and -> si 
+// or -> sau  
+// xor -> true sau false
+// !(a&&b)
+var_dump (true && false);
+echo '<br/>';
+var_dump (true && true);
+echo '<br/>';
+Var_dump (true || false);
+echo '<br/>';
+var_dump (false || true);
+echo '<br/>';
+$a = false && (false || true);
+var_dump ($a);
+echo '<br/>';
+$a = false && false || true;
+var_dump ($a);
+echo '<br/>';
+// alte exemple 
+var_dump (''&& 0);// 0 = false tot timpul; string gol este true tot timpul, adica gilimele goale, paranteze goale, etc;
+echo '<br/>';
+function scrie ($ceva)
+// cum definim o functie; tot timpul incepem cu function dupa care ai nume functie (){}
+// functie cu parametrii function nume functie2($param) {}
+// ca sa apelam o functie, pur si simplu scriem numele ei; nume functie (); sau numefunctie ($param2) sau ($string);
+// functiile pot fi de tip void sau return; 
+{
+    echo $ceva;
+    echo '\n';
+    return false;
+}
+$a = true || scrie ('ceva cu ||');
+echo '<br/>';
+$a = false || scrie ('altceva cu ||');
+echo '<br/>';
+$a = true or scrie ('ceva cu or');
+echo '<br/>';
+$a = true && scrie ('ceva cu &&');
+echo '<br/>';
+// operatorul ternar 
+$a = (2<3)?'text1' : 'text2';
+echo $a;
+echo "\n";
+// $x = (conditie)? 'true' : 'false';
+echo '<br/>';
+$x = 7; 
+$y = ($x%2)? 'impar' : 'par';
+var_dump ($y);
+
+
--- index.php
+++ index.php
@@ -1,3 +1,141 @@
 <?php
 
 echo'Hello Diana!';
+echo '<br/>';
+$a = 10;
+var_dump ($a);
+$b = 2.5;
+var_dump ($b);
+$nume = 'zeciu';
+$prenume = "diana $a";
+echo '<br/>';
+echo $nume;
+echo $prenume;
+echo '<br/>';
+$car = true;
+$bici = false;
+var_dump ($car);
+var_dump ($bici);
+echo '<br/>';
+$array = [1, 'ok', 7.3];
+var_dump ($array);
+echo '<br/>';
+$array2 = [0 =>1, 'car'=> 'VW'];
+var_dump ($array2);
+echo '<br/>';
+$array3 = [$array, $array2];
+var_dump ($array3);
+echo '<br/>';
+class Car {
+    public string $color;
+    function setColor ($newColor) {
+        $this->color = $newColor;
+    }
+}
+$car = new Car();
+$car->setcolor('green');
+echo $car->color;
+var_dump($car);
+echo '<br/>';
+
+$integer = null;
+if (isset($integer)) {
+    print('variabila are valoare');
+} else {
+    print('variabila nu are valoare');
+}
+echo '<br/>';
+$integer = 10;
+if (isset($integer)) {
+    print('variabila are valoare');
+} else {
+    print('variabila nu are valoare');
+}
+echo '<br/>';
+$a = 5;
+$b = 2;
+$c = 'text';
+echo $a + $b;
+echo '<br/>';
+echo $a - $b;
+echo '<br/>';
+echo $a * $b;
+echo '<br/>';
+echo $a / $b; 
+echo '<br/>';
+echo $a % $b;
+echo '<br/>';
+echo $a.$c;
+echo '<br/>';
+echo $a++;
+echo '<br/>';
+echo ++$a;
+echo '<br/>';
+echo --$a;
+echo '<br/>';
+$x = 2;
+$y = 3;
+echo $x**=$y;
+echo '<br/>';
+define ('world', 'Romania');
+echo world;
+echo '<br/>';
+echo M_PI;
+echo '<br/>';
+define ("cars", [
+    "Mitsubishi",
+    "Dacia",
+    "Bmw"
+]);
+echo cars[1]; 
+echo '<br/>';
+
+// Definirea variabilelor 
+$a = 20;
+$b = 5;
+$operatie = ["+", "-", "*", "/"];
+
+// Efectuarea operatiilor
+echo $a+$b;
+echo '<br/>';
+echo $a-$b;
+echo '<br/>';
+echo $a*$b;
+echo '<br/>';
+echo $a/$b;
+echo '<br/>';
+
+// Definirea variabilelor
+$a = 20;
+$b = 5;
+$operatie = "+"; 
+$rezultat = 0;
+
+// Efectuarea operatiei
+
+switch ($operatie) {
+    case "+":
+        $rezultat = $a + $b;
+        break;
+    case "-":
+        $rezultat = $a - $b;
+        break;
+    case "*":
+        $rezultat = $a * $b;
+        break;
+    case "/":
+        if ($b != 0) {
+            $rezultat = $a / $b;
+        } else {
+            echo "Impartirea la 0 nu este permisa!";
+        }
+        break;
+    default:
+        echo "Operatie invalida!";
+}
+
+// Afisarea rezultatului
+echo "Rezultatul: $a $operatie $b = $rezultat";
+
+
+
